d169fad5756d5464575495c14c4a5725167e8d1d
Victor Kano <victorkano27@gmail.com>
2020-01-16 13:20:57 -0300

    database e migration configurados

---
+++ src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  database: 'gympoint',
+  dialect: 'postgres',
+  host: 'localhost',
+  username: 'postgres',
+  password: 'gympoint',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
